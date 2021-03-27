34b7d80f7e3548549ad59f3cf3aacc874bdd531d
DreadTawny <56087087+DreadTawny@users.noreply.github.com>
2021-03-27 19:22:20 +0100

    Websocket basic implementation

--- client-requests.js
+++
@@ -1,1 +0,0 @@
-// sending requests about game state
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 // creating a http server to handle requests about game and websocket connection 
-
-const http = require('http');
-http.createServer( (req,res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Hello world')
-}).listen(8080)
+const http = require('http')
+const static = require('node-static')
+const file = new static.Server('./')
+const app = http.createServer((req, res) => {
+    req.addListener("end", () => {
+        file.serve(req, res)
+    }).resume()
+})
+const io = require('socket.io')(app)
+io.on("connection", socket => {
+    console.log(socket.id)
+    console.log("New connnection")
+    socket.emit("hello", "Welcome to the websocket")
+    socket.on("chat", data => {
+        console.log(data)
+        io.sockets.emit("chat", data)
+    })
+})
+app.listen(3000)
