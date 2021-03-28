eb10cbd76c62bd7766761c52a5e27d65d49a4de0
DreadTawny <56087087+DreadTawny@users.noreply.github.com>
2021-03-28 17:59:41 +0200

    Drawing board

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ const socket = io()
 socket.on("hello", data => {
     console.log(data)
 })
+
 document.getElementById("send").addEventListener("click", () => socket.emit("chat", "Hello, can you hear us") )
 
 socket.on("chat", data => {
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // creating a http server to handle requests about game and websocket connection 
+
 const http = require('http')
 const static = require('node-static')
 const file = new static.Server('./')
@@ -7,9 +8,17 @@ const app = http.createServer((req, res) => {
         file.serve(req, res)
     }).resume()
 })
+let users = { 
+
+}
 const io = require('socket.io')(app)
 io.on("connection", socket => {
-    console.log(socket.id)
+    socket.on("join", data => {
+        users[socket.id] = data
+    }) 
+    socket.on("host", data => {
+        
+    })
     console.log("New connnection")
     socket.emit("hello", "Welcome to the websocket")
     socket.on("chat", data => {
