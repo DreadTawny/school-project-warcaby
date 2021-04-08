8aa1a7e2b5d91164b7a44a6f927bdb0572edd1f2
DreadTawny <56087087+DreadTawny@users.noreply.github.com>
2021-04-08 22:26:11 +0200

    fixing bugs

--- sites/server.js
+++ sites/server.js
@@ -17,48 +17,55 @@ let users = {
 const player = ["white","black" ]
 const io = require('socket.io')(app)
 io.on("connection", socket => {
-    
+    rooms[socket.id.slice(0, 15)] = { "white": socket.id, "black": null}
+    socket.join(socket.id.slice(0,15))
+    socket.emit("showID", socket.id)
     socket.on("join", data => {
-        users[socket.id] = data[0]
-        if(!rooms[data[1]]) rooms[data[1]] = { "white": null, "black": null}
-        if(rooms[data[1]][player[0]] === null) {
-            rooms[data[1]][player[0]] = socket.id
-            socket.emit("playerAssign", player[0])
+        if(data[0]) users[socket.id] = data[0]
+        
+        if(data[1]){
+            let roomID = data[1].slice(0,15)
+            if(!rooms[roomID][player[1]]) {
+                rooms[roomID][player[1]] = socket.id
+            }
+            else {
+                socket.emit("roomOverflow", "Too many players in a room. Please select a new one")
+                socket.leave(data[1].slice(0,15))
+            }
+            socket.join(data[1].slice(0,15))
+            socket.to(data[1]).emit("serverAnnouncment", `User ${users[socket.id]} has joined the room`)
+            let white = rooms[roomID].white
+            let black = rooms[roomID].black
+            io.to(white).emit("playerAssign", player[0])
+            io.to(black).emit("playerAssign", player[1])
         }
-        else if(rooms[data[1]][player[0]] && !rooms[data[1]][player[1]]) {
-            rooms[data[1]][player[1]] = socket.id
-            socket.emit("playerAssign", player[1])
-        }
-        else {
-            socket.emit("roomOverflow", "Too many players in a room. Please select a new one")
-            socket.leave(data[1])
-        }
-        socket.join(data[1])
+        else socket.emit("roomError","No such room exists, please try again")
+       
     }) 
     socket.on("turnEnd", data => {
+        console.log(data.player)
+        console.log(data)
         if(data.player === player[0]){
-            io.to(data.room).emit("startTurn", { map: data.map, player: player[1]}) 
+            console.log("test")
+            console.log(rooms[data.room.slice(0,15)])
+            io.to(data.room.slice(0,15)).emit("startTurn",{map:data.map, player: player[1]})  
         }
         else {
-            io.to(data.room).emit("startTurn",{map:data.map, player: player[0]}) 
+            io.to(data.room.slice(0,15)).emit("startTurn",{map:data.map, player: player[0]}) 
         }
         
     })
    socket.on("message", data => {
-       console.log(data)
-       console.log(users[socket.id])
-       io.to(data[1]).emit("message", {user: users[socket.id], message:data[0]})
+       io.to(data[1].slice(0,15)).emit("message", {user: users[socket.id], message:data[0]})
    })
-   socket.on("disconnect", data => {
-      for(let room in rooms) {
-          socket.leave(room)
-          rooms[room] = null
-          console.log(rooms)
-      }
-   })
+//    socket.on("disconnect", () => {
+//       for(let room in rooms) {
+//           socket.leave(room)
+//           rooms[room] = null
+//       }
+//    })
     socket.emit("hello", "Welcome to the websocket")
     socket.on("chat", data => {
-        console.log(data)
         io.sockets.emit("chat", data)
     })
 })
